55ccb15af6f83a4627de81eb56750755cae1092d
ihabz <ihab.zeedia@gmail.com>
2025-11-18 14:39:54 -0800

    fix conflict in jquery-ui

--- pages/mais_lookup.php
+++ pages/mais_lookup.php
@@ -8,9 +8,9 @@ use GuzzleHttp\Exception\GuzzleException;
 
 $this->injectJSMO();
 ?>
-<link rel="stylesheet" href="https://code.jquery.com/ui/1.14.1/themes/base/jquery-ui.css">
-<link rel="stylesheet" href="/resources/demos/style.css">
-<script src="https://code.jquery.com/ui/1.14.1/jquery-ui.js"></script>
+<!--<link rel="stylesheet" href="https://code.jquery.com/ui/1.14.1/themes/base/jquery-ui.css">-->
+<!--<link rel="stylesheet" href="/resources/demos/style.css">-->
+<!--<script src="https://code.jquery.com/ui/1.14.1/jquery-ui.js"></script>-->
 <style>
     .custom-loader {
         position: fixed;
